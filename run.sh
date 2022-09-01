8c9911029607f6ae9f4255cda125660b3e0d22b8
Sinisa Gajanovic <sinagajanovic@gmail.com>
2022-09-01 14:24:55 +0200

    changed run.sh

--- run.sh
+++ run.sh
@@ -11,4 +11,4 @@ docker network connect superset_default druid & \
 # docker exec -it spark-master spark/bin/spark-submit --jars /driver/postgresql-42.3.5.jar CleanData.py && \
 # docker exec -it spark-master spark/bin/spark-submit --jars /driver/postgresql-42.3.5.jar BatcSpark.py && \
 docker exec -it spark-master spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.2.0 \
- --jars /driver/postgresql-42.3.5.jar /spark/primeri/GEO_kafka_extend-assembly-0.1.0-SNAPSHOT.jar
+ --jars /driver/postgresql-42.3.5.jar /spark/primeri/Stream.py
