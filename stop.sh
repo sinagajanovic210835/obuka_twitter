ee04796d2bb025597faae61862272113b3cfb5c0
Sinisa Gajanovic <sinagajanovic@gmail.com>
2022-09-09 11:22:04 +0200

    modified stop.sh

--- stop.sh
+++ stop.sh
@@ -3,4 +3,5 @@
  docker-compose -f ./docker-compose-nifi.yml down && \
  docker-compose -f ./superset/docker-compose-non-dev.yml down && \
  rm -rf ./spark/Stream/target/ && \
- rm -rf ./spark/Stream/project/target
+ rm -rf ./spark/Stream/project/target && \
+ rm -rf ./spark/Stream/project/project
